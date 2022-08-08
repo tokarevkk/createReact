9587644dd5b864ec89d7ebc5589f1a7850a92bd9
tokarevkk <tokarevkk@gmail.com>
2022-08-08 12:09:08 +0300

    to do list

---
+++ my-app/src/components/TodoList.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { TodoListItem } from './TodoListitem';
+
+interface TodoListProps {
+  todos: Array<Todo>
+  toggleComplete: ToggleComplete
+  onRemoveTodo: RemoveTodo
+  editTodo: EditTodo
+}
+
+export const TodoList: React.FC<TodoListProps> = ({ todos, toggleComplete, onRemoveTodo, editTodo }) => {
+  return (
+    <ul>
+     {todos.map(todo => (
+       <TodoListItem
+          key={todo.text}
+          todo={todo}
+          toggleComplete={toggleComplete}
+          onRemoveTodo={onRemoveTodo}
+          editTodo={editTodo}
+        />
+     ))}
+    </ul>
+  );
+};
+
